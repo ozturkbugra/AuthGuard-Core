3c11f55996742a36a68e291dcbcfdea560509ce4
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-13 19:47:00 +0300

    feat: add register controller and backend logic
    RegisterController created.
    
    UserManager added with Dependency Injection.
    
    User registration with CreateAsync method.
    
    Identity errors added to ModelState.

---
+++ AuthGuardCore/Controllers/RegisterController.cs
@@ -0,0 +1,51 @@
+﻿using AuthGuardCore.Entities;
+using AuthGuardCore.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthGuardCore.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(RegisterUserViewModel model)
+        {
+            AppUser appUser = new()
+            {
+                Name = model.Name,
+                Email = model.Email,
+                Surname = model.SurName,
+                UserName = model.UserName,
+            };
+
+
+            var result = await _userManager.CreateAsync(appUser, model.Password);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError("", item.Description);
+                }
+            }
+
+            return View();
+        }
+    }
+}
---
+++ AuthGuardCore/Models/RegisterUserViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AuthGuardCore.Models
+{
+    public class RegisterUserViewModel
+    {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}

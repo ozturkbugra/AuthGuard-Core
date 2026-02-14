791f6756812a9215f026809c711c9231c12fedc1
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-15 00:00:02 +0300

    feat: implement dynamic category listing with ViewComponent
    Created _MessageCategoryListSidebarComponentPartial to fetch active categories from the database.
    
    Integrated AuthGuardCoreContext via dependency injection to retrieve data.
    
    Developed the default view to dynamically render category names and icons using a foreach loop.

---
+++ AuthGuardCore/ViewComponents/MessageViewComponents/_MessageCategoryListSidebarComponentPartial.cs
@@ -0,0 +1,23 @@
+﻿using AuthGuardCore.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthGuardCore.ViewComponents.MessageViewComponents
+{
+    public class _MessageCategoryListSidebarComponentPartial : ViewComponent
+    {
+        private readonly AuthGuardCoreContext _context;
+
+        public _MessageCategoryListSidebarComponentPartial(AuthGuardCoreContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _context.Categories.Where(x=> x.Status == true).OrderBy(x=> x.Name).ToListAsync();
+            return View(values);
+        }
+
+    }
+}

e8c4c7e622cec5d6659f69317cd2da98d43b122d
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-14 22:39:44 +0300

    feat: add login controller and backend logic
    LoginController created with SignInManager integration.
    
    Implemented PasswordSignInAsync method for user authentication.
    
    Handled login results and redirection to the profile page.

--- AuthGuardCore/Controllers/LoginController.cs
+++ AuthGuardCore/Controllers/LoginController.cs
@@ -1,10 +1,19 @@
-﻿using AuthGuardCore.Models;
+﻿using AuthGuardCore.Entities;
+using AuthGuardCore.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AuthGuardCore.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public LoginController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -13,6 +22,11 @@ namespace AuthGuardCore.Controllers
         [HttpPost]
         public async Task<IActionResult> Index(LoginUserViewModel model)
         {
+            var result = await _signInManager.PasswordSignInAsync(model.UserName, model.Password,true,true);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Profile", "MyProfile");
+            }
             return View();
         }
     }

801c3e2a3eb8ab0b16bcf26cf4c018229b7b7384
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-14 23:21:38 +0300

    feat: implement Inbox page and Sidebar ViewComponent
    The UI design for the Inbox page is completed.
    
    Created _MessageSidebarComponentPartial for reusable navigation.
    
    Static message list added to the table for testing the layout.

---
+++ AuthGuardCore/ViewComponents/MessageViewComponents/_MessageSidebarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuthGuardCore.ViewComponents.MessageViewComponents
+{
+    public class _MessageSidebarComponentPartial : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}

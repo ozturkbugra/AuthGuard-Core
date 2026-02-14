6a0fadb4c200b06d82cc92f17adc308e7de2dfce
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-15 01:06:36 +0300

    feat: implement Sendbox message listing with static email filtering
    Developed Sendbox action in MessageController to sender messages from the database.
    
    Applied a static email filter (ztrk1212@gmail.com) to SenderEmail for testing the message flow.
    
    Integrated AuthGuardCoreContext to fetch and display receiver-specific data in the view.

--- AuthGuardCore/Controllers/MessageController.cs
+++ AuthGuardCore/Controllers/MessageController.cs
@@ -18,5 +18,12 @@ namespace AuthGuardCore.Controllers
             var values = await _context.Messages.Where(x => x.RecieverEmail == "ztrk1212@gmail.com").ToListAsync();
             return View(values);
         }
+
+
+        public async Task<IActionResult> Sendbox()
+        {
+            var values = await _context.Messages.Where(x => x.SenderEmail == "ztrk1212@gmail.com").ToListAsync();
+            return View(values);
+        }
     }
 }

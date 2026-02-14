be71569ff77fa5ea3274e26a2351bcf2ab5a9a42
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-15 00:32:14 +0300

    feat: implement Inbox message listing with static email filtering
    Developed Inbox action in MessageController to retrieve messages from the database.
    
    Applied a static email filter (ztrk1212@gmail.com) to RecieverEmail for testing the message flow.
    
    Integrated AuthGuardCoreContext to fetch and display receiver-specific data in the view.

--- AuthGuardCore/Controllers/MessageController.cs
+++ AuthGuardCore/Controllers/MessageController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AuthGuardCore.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AuthGuardCore.Controllers
 {
     public class MessageController : Controller
     {
-        public IActionResult Inbox()
+        private readonly AuthGuardCoreContext _context;
+
+        public MessageController(AuthGuardCoreContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Inbox()
+        {
+            var values = await _context.Messages.Where(x => x.RecieverEmail == "ztrk1212@gmail.com").ToListAsync();
+            return View(values);
         }
     }
 }

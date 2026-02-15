06d92e92bb2231eb67d1b7b0c0d23c43909648fc
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-15 21:35:58 +0300

    feat: complete role CRUD operations with Delete and Update logic
    Implemented DeleteRole action to remove identity roles asynchronously by their unique ID.
    
    Developed UpdateRole GET and POST actions to enable role name modifications.
    
    Utilized UpdateRoleViewModel to ensure secure and structured data transfer during updates.
    
    Integrated RoleManager.UpdateAsync and DeleteAsync for robust database synchronization.

--- AuthGuardCore/Controllers/RoleController.cs
+++ AuthGuardCore/Controllers/RoleController.cs
@@ -23,6 +23,38 @@ namespace AuthGuardCore.Controllers
             return View(values);
         }
 
+
+        public async Task<IActionResult> DeleteRole(string id)
+        {
+            var values = await _roleManager.Roles.FirstOrDefaultAsync(x=> x.Id == id);
+            await _roleManager.DeleteAsync(values);
+            return RedirectToAction("RoleList");
+        }
+
+        public async Task<IActionResult> UpdateRole(string id)
+        {
+            var values = await _roleManager.Roles.FirstOrDefaultAsync(x=> x.Id == id);
+            UpdateRoleViewModel model = new()
+            {
+                RoleID = values.Id,
+                RoleName = values.Name
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateRole(UpdateRoleViewModel model)
+        {
+            var values = await _roleManager.Roles.FirstOrDefaultAsync(x => x.Id == model.RoleID);
+
+            values.Name = model.RoleName;
+            
+            await _roleManager.UpdateAsync(values);
+            return RedirectToAction("RoleList");
+        }
+
+
+
         public async Task<IActionResult> CreateRole()
         {
             return View();
---
+++ AuthGuardCore/Models/UpdateRoleViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthGuardCore.Models
+{
+    public class UpdateRoleViewModel
+    {
+        public string RoleID { get; set; }
+        public string RoleName { get; set; }
+    }
+}

634535bd447629a3ed496546804d514a711ae07f
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-17 18:50:02 +0300

    feat: list user comments with Eager Loading and forum-style UI
    Implemented UserComments action to fetch all comments from the database.
    
    Applied Include(x => x.AppUser) to efficiently load related user details alongside comments.
    
    Developed a forum-style View using a foreach loop to display user names, dates, and comment texts.
    
    Formatted comment metadata to show full name and date-time in a user-friendly way.

---
+++ AuthGuardCore/Controllers/CommentController.cs
@@ -0,0 +1,28 @@
+﻿using AuthGuardCore.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthGuardCore.Controllers
+{
+    public class CommentController : Controller
+    {
+        private readonly AuthGuardCoreContext _context;
+
+        public CommentController(AuthGuardCoreContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> UserComments()
+        {
+            var values = await _context.Comments.Include(x => x.AppUser).ToListAsync(); 
+            return View(values);
+        }
+
+        public async Task<IActionResult> UserCommentList()
+        {
+            var values = await _context.Comments.Include(x => x.AppUser).ToListAsync(); 
+            return View(values);
+        }
+    }
+}
